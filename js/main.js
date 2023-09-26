a2e9d280c8707c3787f65ba77736abc58c504740
Gabriel Raiol <raiol.gabriel@hotmail.com>
2023-09-26 10:37:21 -0300

    changing addEventListener to onclick function

--- js/main.js
+++ js/main.js
@@ -2,17 +2,22 @@ let teclas = document.querySelectorAll(".tecla");
 
 teclas.forEach((tecla) => {
 
-    tecla.addEventListener("click", () => {
-
+    tecla.onclick = () => {
         const teclaId = tecla.id;
         getSound(teclaId);
-    });
+    };
 });
-
-
 
 function getSound(id) {
     const soundId = `#som_${id}`;
     let audio = document.querySelector(soundId).play();
     return audio;
 };
+
+const button = document.querySelector("button");
+
+/*
+button.onclick = (e) => {
+    console.log("ei caralho");
+};
+*/
